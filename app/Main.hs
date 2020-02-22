87ed4e5a3f73a6d9314a95ac625352464b481eb3
subta90 <the.bird.nobility298@gmail.com>
2020-02-22 17:46:30 +0900

    終了判定を追加

--- app/Main.hs
+++ app/Main.hs
@@ -6,20 +6,23 @@ module Main where
 import Lib
 
 type BoardIndex = Int
-data Piece = Maru | Batsu | None
+data Piece = Maru | Batsu | None deriving Eq
 type Board = [(BoardIndex, Piece)]
 
 main :: IO ()
-main = someFunc     
+main = someFunc
 
 initBoard :: BoardIndex -> Board
 initBoard 0 = [(0, None)]
 initBoard index = [(index, None)] ++ initBoard (index - 1)
 
-markBoard :: Board -> BoardIndex -> Piece -> Board 
+markBoard :: Board -> BoardIndex -> Piece -> Board
 markBoard board index piece = map (markBoardUnit index piece) board
 
 markBoardUnit :: BoardIndex -> Piece -> (BoardIndex, Piece) ->  (BoardIndex, Piece)
 markBoardUnit index piece unit
     | index == fst unit = (index, piece)
     | otherwise = unit
+
+isFinished :: Board -> Bool
+isFinished board = not $ any ((==None).snd) board
