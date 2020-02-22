f8887033b7e570f4efc61da929f6b796706bb135
subta90 <the.bird.nobility298@gmail.com>
2020-02-22 16:17:15 +0900

    盤面初期化処理を追加

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
 module Main where
 
 import Lib
 
+type BoardIndex = Int
+data Piece = Maru | Batsu | None
+type Board = [(Int, Piece)]
+
 main :: IO ()
 main = someFunc
+
+initBoard :: BoardIndex -> Board
+initBoard 0 = [(0, None)]
+initBoard index = [(index, None)] ++ initBoard (index - 1)
