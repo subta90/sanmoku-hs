831b7dd76bd2d42a92258c08050b11a8f76a4b56
subta90 <the.bird.nobility298@gmail.com>
2020-02-23 13:27:51 +0900

    勝利条件判定処理を追加

--- app/Main.hs
+++ app/Main.hs
@@ -30,3 +30,34 @@ isFinished board = not $ any ((==None).snd) board
 
 extractIndexList :: Board -> Piece -> [BoardIndex]
 extractIndexList board piece = map fst $ filter ((==piece).snd) board
+
+judgeWinner :: Board -> Piece
+judgeWinner board
+    | isLineCompleted $ extractIndexList board Maru = Maru
+    | isLineCompleted $ extractIndexList board Batsu = Batsu
+    | otherwise = None
+
+isLineCompleted :: [BoardIndex] -> Bool 
+isLineCompleted indexList
+    | isRowLineCompleted indexList = True
+    | isColumnLineCompleted indexList = True
+    | isDiagonallyLineCompleted indexList = True
+    | otherwise = False
+
+isRowLineCompleted :: [BoardIndex] -> Bool
+isRowLineCompleted [0,1,2] = True
+isRowLineCompleted [3,4,5] = True
+isRowLineCompleted [6,7,8] = True
+isRowLineCompleted _ = False
+
+isColumnLineCompleted :: [BoardIndex] -> Bool
+isColumnLineCompleted [0,3,6] = True
+isColumnLineCompleted [1,4,7] = True 
+isColumnLineCompleted [2,5,8] = True
+isColumnLineCompleted _ = False
+
+isDiagonallyLineCompleted :: [BoardIndex] -> Bool
+isDiagonallyLineCompleted [0,4,8] = True
+isDiagonallyLineCompleted [2,4,6] = True
+isDiagonallyLineCompleted _ = False
+
